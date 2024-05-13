129f4a3472f559b79eddd0d81f2db952b90697b8
nima-siboni <nima.siboni@gmail.com>
2024-05-13 13:52:51 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup file for Furnace."""
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='PythonDesignPattern',
+    packages=find_packages(),
+)
